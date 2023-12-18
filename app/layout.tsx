db970146966ae5d02c1e6808d28bbe2dafba37ba
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-18 15:53:06 -0300

    create login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,22 @@
 import type { Metadata } from 'next'
-import './globals.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './globals.scss'
+import { Nunito } from 'next/font/google'
+
+const nunito = Nunito({
+    subsets: ['latin'],
+    weight: ['400', '700']
+})
 
 export const metadata: Metadata = {
   title: 'Alimentar com Saúde',
   description: 'Rede Social para quem quer se alimentar com saúde e ter uma vida saudável. Desenvolvido por João Mozelli Neto',
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({children,}: {children: React.ReactNode}) {
   return (
     <html lang="pt-br">
-      <body>{children}</body>
+      <body style={ nunito.style }>{children}</body>
     </html>
   )
 }
---
+++ app/login/page.tsx
@@ -0,0 +1,50 @@
+
+import styles from './login.module.scss'
+
+export default function Login() {
+    return (
+        <div className={ styles.login }>
+            <header>
+                <div className="row">
+                    <div className="col-lg-3">
+                        <h1>Alimentar com Saúde</h1>
+                    </div>
+                    <div className="col-lg-7">
+
+                    </div>
+                    <div className="col-lg-2">
+        
+                    </div>
+                </div>
+            </header>
+            <section className={ styles.formulary }>
+                <div className="container">
+                    <div className={ styles.loginFormulary }>
+                        <div className="row justify-content-center p-3">
+                            <div className="col-12">
+                                <h2 className={ styles.formTitle }>Login</h2>
+                            </div>
+                            <div className="col-12">
+                                <div className="col mb-3">
+                                    <input type="text" className="form-control" placeholder='E-mail' />
+                                </div>
+                                <div className="col mb-3">
+                                    <input type="password" className='form-control' placeholder='Senha' />
+                                </div>
+                                <div className="col d-grid">
+                                    <button type='button' className='btn btn-primary'>Entrar</button>
+                                </div>
+                                <div className="col my-3">
+                                    <a href='#' className={ styles.forgotMyPassword }>Esqueci minha senha</a>
+                                </div>
+                                <div className="col my-3 text-center">
+                                    <a href="#" className={ styles.signUp }>Quero me cadastrar!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
