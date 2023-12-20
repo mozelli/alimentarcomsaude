31b4b06dd2ccc2292b08c0ac83ade6afdfbd9011
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-20 14:11:36 -0300

    create login formulary validation rules and remember me checkbox

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,40 @@
 
+'use client';
 import Link from 'next/link'
+import React, { useState } from 'react'
 import styles from './login.module.scss'
 import Header from '../components/header/page'
 import Footer from '../components/footer/page'
 
 export default function Login() {
+
+    const [ email, setEmail] = useState('');
+    const [ password, setPassword ] = useState('');
+    const [ rememberMe, setRememberMe ] = useState(false);
+    const [ emailHelpBlock, setEmailHelpBlock ] = useState(true);
+    const [ passwordHelpBlock, setPasswordHelpBlock ] = useState(true);
+
+    const handleLoginForm = (event: any) => {
+        event.preventDefault();
+        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const regexSenha = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*]{8,}$/;
+
+        if(regexEmail.test(email)) {
+            setEmailHelpBlock(true);  
+            if(regexSenha.test(password)) {
+                setPasswordHelpBlock(true);  
+                console.log({
+                    email, password, rememberMe
+                });
+            } else {
+                setPasswordHelpBlock(false);
+            }              
+        } else {
+            setEmailHelpBlock(false);
+        }
+    }
+
+
     return (
         <div className={ styles.login }>
             <Header />
@@ -16,15 +46,57 @@ export default function Login() {
                                 <h2 className={ styles.formTitle }>Login</h2>
                             </div>
                             <div className="col-12">
-                                <div className="col mb-3">
-                                    <input type="text" className="form-control" placeholder='E-mail' />
-                                </div>
-                                <div className="col mb-3">
-                                    <input type="password" className='form-control' placeholder='Senha' />
-                                </div>
-                                <div className="col d-grid">
-                                    <button type='button' className='btn btn-primary'>Entrar</button>
-                                </div>
+                                <form onSubmit={handleLoginForm}>
+                                    <div className="col mb-3">
+                                        <input 
+                                            type="email" 
+                                            className="form-control" 
+                                            placeholder='E-mail' 
+                                            value={ email }
+                                            onChange={ (event) => setEmail(event.target.value) } />
+                                        <div 
+                                            id="emailHelpBlock" 
+                                            className="form-text text-danger" 
+                                            hidden={ emailHelpBlock }
+                                            >
+                                            Informe um e-mail válido.
+                                        </div>
+                                    </div>
+                                    <div className="col mb-3">
+                                        <input 
+                                            type="password" 
+                                            className='form-control' 
+                                            placeholder='Senha' 
+                                            value={ password } 
+                                            onChange={ (event) => setPassword(event.target.value) } />
+                                        <div 
+                                            id="passwordHelpBlock" 
+                                            className="form-text text-danger" 
+                                            hidden={ passwordHelpBlock }
+                                            >
+                                            Sua senha deve conter letras e números.
+                                        </div>
+                                    </div>
+                                    <div className="col mb-3">
+                                        <div className="form-check">
+                                            <input 
+                                                className="form-check-input" 
+                                                type="checkbox" 
+                                                value= 'rememberMe'
+                                                name='rememberMe'
+                                                id="rememberMe" 
+                                                onChange={ (event) => setRememberMe(event.target.checked) }
+                                                checked={ rememberMe }
+                                                />
+                                            <label className="form-check-label" htmlFor="agreeTerms">
+                                                Lembrar-me neste dispositivo
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="col d-grid">
+                                        <button type='submit' className='btn btn-primary'>Entrar</button>
+                                    </div>
+                                </form>
                                 <div className="col my-3">
                                     <a href='#' className={ styles.forgotMyPassword }>Esqueci minha senha</a>
                                 </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-//import Login from './login/page'
+import Login from './login/page'
 import styles from './page.module.scss'
 //import SignUp from './signup/page'
 //import Header from './components/header/page'
-import Dashboard from './dashboard/page'
+//import Dashboard from './dashboard/page'
 
 export default function Home() {
   return (
     <main>
-      <Dashboard />
+      <Login />
     </main>
   )
 }
