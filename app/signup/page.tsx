61d3e25581b02dc632d5289fb68c824201807148
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 16:53:58 -0300

    implements react rook forms

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,26 +1,66 @@
 'use client';
-import styles from './signup.module.scss'
-import { useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
 import Header from '../components/header/page';
 import Footer from '../components/footer/page';
+import styles from './signup.module.scss'
+
+interface IFormInput {
+    name: string,
+    email: string,
+    birthday: string,
+    gender: string,
+    password: string,
+    rePassword: string,
+    terms: boolean
+}
 
 const SignUp = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [gender, setGender] = useState('');
-    const [birthday, setBirthday] = useState('');
-    const [agreeTerms, setAgreeTerms] = useState(false);
-    const [show, setShow] = useState(true);
+    const { register, handleSubmit, formState: {errors}, getValues } = useForm<IFormInput>();
+    const router = useRouter();
+    // const { name } = useContext(AuthContext);
 
-    const Terms = () => {
-        setAgreeTerms(!agreeTerms);
-    }
+    // const dataRegex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
 
-    const formHandle = (event: any) => {
-        event.preventDefault();
-        console.log({name, email, gender, birthday, agreeTerms});
+    const checkAge = (date: string): boolean => {
+        const hoje = new Date();
+        const nascimento = new Date(date.replace(/(\d{2})\/(\d{2})\/(\d{4})/, '$2/$1/$3')); // Invertendo o formato para criar a data correta
+      
+        const diferencaAnos = hoje.getFullYear() - nascimento.getFullYear();
+        const mesAtualMenorQueNascimento = hoje.getMonth() < nascimento.getMonth();
+        const mesmoMesMasDiaAtualMenorQueNascimento =
+          hoje.getMonth() === 
+          nascimento.getMonth() 
+          && hoje.getDate() < 
+          nascimento.getDate();
+      
+        return diferencaAnos > 18 || (diferencaAnos === 18 && (!mesAtualMenorQueNascimento || mesmoMesMasDiaAtualMenorQueNascimento));
+    };
+      
+
+    function onSubmit(data: any) {
+        const gmt = new Date(data.birthday);
+
+        axios.post('http://localhost:3333/addNewUser', {
+            name: data.name,
+            email: data.email,
+            birthday: gmt,
+            gender: data.gender,
+            password: data.password
+        })
+        .then((response) => {
+            console.log(response);
+            if(response.status === 201) {
+                router.replace('/dashboard');
+            }
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar formulário de registro.", { error });
+        });
+        // setOutput(JSON.stringify(data, null, 2))
     }
 
     return (
@@ -28,79 +68,174 @@ const SignUp = () => {
             <Header />
             <section className={ styles.subscribe }>
                 <div className="container">
-                    <form className={ styles.singupFormulary } onSubmit={formHandle}>
+                    <form className={ styles.singupFormulary }>
                         <div className="row justify-content-center p-3">
                             <div className="col-12">
                                 <h2 className={ styles.formTitle }>Cadastro</h2>
-                            </div>
-                            <div className="alert alert-danger" role="alert" hidden={show}>
-                                Você deve aceitar os termos de política e privacidade!
                             </div>
                             <div className="col-12">
                                 <div className="col mb-3">
                                     <input 
                                         type="text" 
-                                        value={ name } 
                                         aria-label="Nome"
-                                        onChange={ (e) => setName(e.target.value) } 
-                                        className="form-control" 
-                                        placeholder='Nome' 
-                                        required
+                                        className={ `form-control ${errors?.name && "is-invalid"}`}
+                                        placeholder='Nome'
+                                        { ...register('name', {
+                                            required: true, 
+                                            minLength: 3 
+                                            }) 
+                                        } 
                                     />
+                                    { 
+                                        errors?.name?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe seu nome.
+                                        </div>
+                                    }
+
                                 </div>
                                 <div className="col mb-3">
                                     <input 
-                                        type="email" 
-                                        value={email} onChange={ (e) => setEmail(e.target.value) } 
+                                        type="text" 
                                         aria-label="email"
-                                        className="form-control" 
-                                        placeholder='E-mail' 
-                                        required
+                                        className={ `form-control ${errors?.email && "is-invalid"}`}
+                                        placeholder='E-mail'
+                                        { ...register('email', {
+                                            required: true, 
+                                            pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+                                            }) 
+                                        } 
                                     />
+                                    { 
+                                        errors?.email?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe seu e-mail.
+                                        </div>
+                                    }
+                                    { 
+                                        errors?.email?.type === 'pattern' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe um e-mail válido!
+                                        </div>
+                                    }
                                 </div>
                                 <div className="col mb-3">
                                     <input 
-                                        type="date" 
-                                        id='date' value={ birthday } onChange={(e) => setBirthday(e.target.value) } 
+                                        type="date"   
                                         aria-label="Data de nascimento"
-                                        className='form-control' 
+                                        className={ `form-control ${errors?.birthday && "is-invalid"}`}
                                         placeholder='Data de nascimento' 
-                                        min="2005-12-20"
-                                        required
+                                        { ...register('birthday', { validate: (value) => {
+                                            return checkAge(value);
+                                        } }) 
+                                        }
                                     />
+                                    { 
+                                        errors?.birthday?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe sua data de nascimento.
+                                        </div>
+                                    }
+                                    { 
+                                        errors?.birthday?.type === 'validate' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Você deve ter mais de 18 anos.
+                                        </div>
+                                    }
                                 </div>
                                 <div className="col mb-3">
                                     <select 
                                         className="form-select" 
                                         aria-label="Gênero" 
-                                        value={ gender } 
-                                        onChange={ (e) => setGender(e.target.value) } 
-                                        required
+                                        { ...register('gender', {required: true}) }
                                         >
-                                        <option defaultValue={""}>Escolha</option>
-                                        <option value="masculino">Masculino</option>
-                                        <option value="feminino">Feminino</option>
-                                        <option value="não informado">Prefiro não declarar</option>
+                                        <option>Gênero</option>
+                                        <option>Masculino</option>
+                                        <option>Feminino</option>
+                                        <option>Prefiro não informar</option>
                                     </select>
+                                </div>
+                                <div className="col mb-3">
+                                    <input 
+                                        type="password" 
+                                        aria-label="senha"
+                                        className={ `form-control ${errors?.password && "is-invalid"}`}
+                                        placeholder='Senha'
+                                        { ...register('password', {
+                                            required: true,
+                                            minLength: 8,
+                                            pattern: /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d!@#$%^&*]{8,}$/
+                                        }) } 
+                                    />
+                                    { 
+                                        errors?.password?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe uma senha.
+                                        </div>
+                                    }
+                                </div>
+                                <div className="col mb-3">
+                                    <input 
+                                        type="password" 
+                                        aria-label="senha"
+                                        className={ `form-control ${errors?.rePassword && "is-invalid"}`}
+                                        placeholder='Repita a senha'
+                                        { ...register('rePassword', { required: true, validate: (value: string) => {
+                                            const { password } = getValues();
+                                            return password === value;
+                                        } }) } 
+                                    />
+                                    { 
+                                        errors?.rePassword?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            Informe a senha novamente.
+                                        </div>
+                                    }
+                                    { 
+                                        errors?.rePassword?.type === 'validate' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            As senhas devem ser iguais!
+                                        </div>
+                                    }
                                 </div>
                                 <div className="col mb-3">
                                     <div className="form-check">
                                         <input 
-                                            className="form-check-input" 
+                                            id='terms'
+                                            className={ `form-check-input ${errors?.terms && "is-invalid"}`}
+                                            // className="form-check-input" 
                                             type="checkbox" 
-                                            value= "terms" 
-                                            name='agreeTerms'
-                                            id="agreeTerms" 
-                                            onChange={() => Terms()} 
-                                            required 
+                                            { ...register('terms', {
+                                                required: true
+                                            }) }
                                             />
-                                        <label className="form-check-label" htmlFor="agreeTerms">
+                                        <label className="form-check-label" htmlFor="terms">
                                             Concordo com os termos de serviço e as políticas de privacidade.
                                         </label>
+                                        { 
+                                        errors?.terms?.type === 'required' 
+                                        &&  
+                                        <div className="invalid-feedback">
+                                            <strong>Você deve concordar com os termos!</strong>
                                         </div>
+                                    }
                                     </div>
+                                </div>
                                 <div className="col d-grid">
-                                    <button type='submit' className='btn btn-primary'>Cadastrar</button>
+                                    <button 
+                                        type='submit' 
+                                        className='btn btn-primary' 
+                                        onClick={handleSubmit(onSubmit)}>
+                                            Cadastrar
+                                    </button>
                                 </div>
                                 <div className="col mt-3 text-center">
                                     <Link href={"/login"} className={ styles.backToLogin }>Voltar para login</Link>
