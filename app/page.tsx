cf2c1293f109e366ff2d7c21a0dae4b4f2ce9dde
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 16:48:12 -0300

    clear code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
+
 import Login from './login/page'
-import styles from './page.module.scss'
+//import styles from './page.module.scss'
 //import SignUp from './signup/page'
-//import Header from './components/header/page'
-//import Dashboard from './dashboard/page'
 
 export default function Home() {
   return (
