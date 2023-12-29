635e4f003da0c0d7ff67799fe2f21ad27186e2cb
João Mozelli Neto <joaomozelli@hotmail.com>
2023-12-29 16:49:03 -0300

    update header

--- app/components/header/page.tsx
+++ app/components/header/page.tsx
@@ -1,22 +1,29 @@
-import styles from './header.module.scss'
+//import styles from './header.module.scss'
+
+import Link from "next/link";
 
 const Header = () => {
     return (
-        <div className={ styles.header }>
-            <header>
-                <div className="row">
-                    <div className="col-lg-3">
-                        <h1>Alimentar com Saúde</h1>
-                    </div>
-                    <div className="col-lg-7">
+        <nav className="navbar bg-success">
+            <div className="container-fluid">
+                <Link href="/" className="navbar-brand text-white">Alimentar com Saúde</Link>
+            </div>
+        </nav>
+        // <div className={ styles.header }>
+        //     <header>
+        //         <div className="row">
+        //             <div className="col-lg-3">
+        //                 <h1>Alimentar com Saúde</h1>
+        //             </div>
+        //             <div className="col-lg-7">
 
-                    </div>
-                    <div className="col-lg-2">
+        //             </div>
+        //             <div className="col-lg-2">
             
-                    </div>
-                </div>
-            </header>
-        </div>
+        //             </div>
+        //         </div>
+        //     </header>
+        // </div>
     )
 }
 
